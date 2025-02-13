35b98389c9d9a403dd4e83039fc4ecc447215149
mikeletzu <120392721+mikeletzu@users.noreply.github.com>
2025-02-13 15:12:57 +0100

    P3 casi

---
+++ DSI_P03/Assets/p3b.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class p3b : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        VisualElement verde = root.Q("ContenedorVerde");
+        VisualElement azul = root.Q("ContenedorAzul");
+        VisualElement amarillo = root.Q("ItemAmarillo");
+
+        verde.RegisterCallback<MouseDownEvent>(
+            ev =>
+            {
+                Debug.Log("“Contenedor Verde. Fase: " + ev.propagationPhase);
+                Debug.Log("“Contenedor Verde. Current target: " + (ev.currentTarget as VisualElement).name);
+                Debug.Log("“Contenedor Verde. Target: " + (ev.target as VisualElement).name);
+            }/*, TrickleDown.NoTrickleDown*/);
+
+        azul.RegisterCallback<MouseDownEvent>(
+            ev =>
+            {
+                Debug.Log("“Contenedor Azul. Fase: " + ev.propagationPhase);
+                Debug.Log("“Contenedor Azul. Current target: " + (ev.currentTarget as VisualElement).name);
+                Debug.Log("“Contenedor Azul. Target: " + (ev.target as VisualElement).name);
+            });
+
+        amarillo.RegisterCallback<MouseDownEvent>(
+            ev =>
+            {
+                Debug.Log("Contenedor Amarillo. Fase: " + ev.propagationPhase);
+                Debug.Log("“Contenedor Amarillo. Current target: " + (ev.currentTarget as VisualElement).name);
+                Debug.Log("“Contenedor Amarillo. Target: " + (ev.target as VisualElement).name);
+            });
+    }
+}
+
