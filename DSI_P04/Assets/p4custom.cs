da530171b8da9dfaaba6c77a097bf69f39f4e3a3
nievesag <nievalon@ucm.es>
2025-02-17 15:14:27 +0100

    queda el string

--- DSI_P04/Assets/p4custom.cs
+++ DSI_P04/Assets/p4custom.cs
@@ -3,9 +3,175 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.UIElements.VisualElement;
 using UnityEngine.UIElements;
-
 public class p4custom : VisualElement
 {
+    VisualElement dif0 = new VisualElement();
+    VisualElement dif1 = new VisualElement();
+    VisualElement dif2 = new VisualElement();
+    VisualElement dif3 = new VisualElement();
+    VisualElement dif4 = new VisualElement();
+    VisualElement dif5 = new VisualElement();
+
+    private Color gris = new Color(0.4f, 0.4f, 0.4f);
+    private Color blanco = new Color(1f, 1f, 1f);
+
+    Sprite img = Resources.Load<Sprite>("Imagenes/georgia");
+
+    int estado;
+    public int Estado
+    {
+        get => estado;
+        set
+        {
+            estado = value;
+            cambiarDificultad();
+        }
+    }
+
+    string icon;
+    public string Icon
+    {
+        get => icon;
+        set
+        {
+            icon = value;
+            cambiarDificultad();
+        }
+    }
+
+    void cambiarDificultad()
+    {
+        // imagenes
+        dif0.style.backgroundImage = new StyleBackground(img);
+        dif1.style.backgroundImage = new StyleBackground(img);
+        dif2.style.backgroundImage = new StyleBackground(img);
+        dif3.style.backgroundImage = new StyleBackground(img);
+        dif4.style.backgroundImage = new StyleBackground(img);
+        dif5.style.backgroundImage = new StyleBackground(img);
+
+        // colores
+        dif0.style.unityBackgroundImageTintColor = gris;
+        dif1.style.unityBackgroundImageTintColor = gris;
+        dif2.style.unityBackgroundImageTintColor = gris;
+        dif3.style.unityBackgroundImageTintColor = gris;
+        dif4.style.unityBackgroundImageTintColor = gris;
+        dif5.style.unityBackgroundImageTintColor = gris;
+
+        // --- cambios de estado ---
+        if (Estado == 0)
+        {
+            dif0.style.unityBackgroundImageTintColor = blanco;
+            dif1.style.unityBackgroundImageTintColor = gris;
+            dif2.style.unityBackgroundImageTintColor = gris;
+            dif3.style.unityBackgroundImageTintColor = gris;
+            dif4.style.unityBackgroundImageTintColor = gris;
+            dif5.style.unityBackgroundImageTintColor = gris;
+        }
+
+        if (Estado == 1)
+        {
+            dif0.style.unityBackgroundImageTintColor = blanco;
+            dif1.style.unityBackgroundImageTintColor = blanco;
+            dif2.style.unityBackgroundImageTintColor = gris;
+            dif3.style.unityBackgroundImageTintColor = gris;
+            dif4.style.unityBackgroundImageTintColor = gris;
+            dif5.style.unityBackgroundImageTintColor = gris;
+        }
+
+        if (Estado == 2)
+        {
+            dif0.style.unityBackgroundImageTintColor = blanco;
+            dif1.style.unityBackgroundImageTintColor = blanco;
+            dif2.style.unityBackgroundImageTintColor = blanco;
+            dif3.style.unityBackgroundImageTintColor = gris;
+            dif4.style.unityBackgroundImageTintColor = gris;
+            dif5.style.unityBackgroundImageTintColor = gris;
+        }
+
+        if (Estado == 3)
+        {
+            dif0.style.unityBackgroundImageTintColor = blanco;
+            dif1.style.unityBackgroundImageTintColor = blanco;
+            dif2.style.unityBackgroundImageTintColor = blanco;
+            dif3.style.unityBackgroundImageTintColor = blanco;
+            dif4.style.unityBackgroundImageTintColor = gris;
+            dif5.style.unityBackgroundImageTintColor = gris;
+        }
+
+        if (Estado == 4)
+        {
+            dif0.style.unityBackgroundImageTintColor = blanco;
+            dif1.style.unityBackgroundImageTintColor = blanco;
+            dif2.style.unityBackgroundImageTintColor = blanco;
+            dif3.style.unityBackgroundImageTintColor = blanco;
+            dif4.style.unityBackgroundImageTintColor = blanco;
+            dif5.style.unityBackgroundImageTintColor = gris;
+        }
+
+        if (Estado == 5)
+        {
+            dif0.style.unityBackgroundImageTintColor = blanco;
+            dif1.style.unityBackgroundImageTintColor = blanco;
+            dif2.style.unityBackgroundImageTintColor = blanco;
+            dif3.style.unityBackgroundImageTintColor = blanco;
+            dif4.style.unityBackgroundImageTintColor = blanco;
+            dif5.style.unityBackgroundImageTintColor = blanco;
+        }
+    }
+
+    public new class UxmlFactory : UxmlFactory<p4custom, UxmlTraits> { };
+
+    public new class UxmlTraits : VisualElement.UxmlTraits
+    {
+        UxmlIntAttributeDescription myEstado = new UxmlIntAttributeDescription { name = "estado", defaultValue = 0 };
+
+        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+        {
+            base.Init(ve, bag, cc);
+            var dificultad = ve as p4custom;
+            dificultad.Estado = myEstado.GetValueFromBag(bag, cc);
+            Debug.Log("dificultad.Estado: " + dificultad.Estado);
+        }
+    }
+
+    public p4custom()
+    {
+        dif0.style.width = 100;
+        dif0.style.height = 100;
+
+        dif1.style.width = 100;
+        dif1.style.height = 100;
+
+        dif2.style.width = 100;
+        dif2.style.height = 100;
+
+        dif3.style.width = 100;
+        dif3.style.height = 100;
+
+        dif4.style.width = 100;
+        dif4.style.height = 100;
+
+        dif5.style.width = 100;
+        dif5.style.height = 100;
+
+        styleSheets.Add(Resources.Load<StyleSheet>("p4custom"));
+
+        dif0.AddToClassList("panel_round");
+        dif1.AddToClassList("panel_round");
+        dif2.AddToClassList("panel_round");
+        dif3.AddToClassList("panel_round");
+        dif4.AddToClassList("panel_round");
+        dif5.AddToClassList("panel_round");
+
+        hierarchy.Add(dif0);
+        hierarchy.Add(dif1);
+        hierarchy.Add(dif2);
+        hierarchy.Add(dif3);
+        hierarchy.Add(dif4);
+        hierarchy.Add(dif5);
+    }
+
+    /*
     VisualElement panelRojo = new VisualElement();
     VisualElement panelAmbar = new VisualElement();
     VisualElement panelVerde = new VisualElement();
@@ -21,8 +187,9 @@ public class p4custom : VisualElement
             encenderColor();
         }
     }
-        void encenderColor()
-        {
+
+    void encenderColor()
+    {
         panelRojo.style.backgroundColor = new Color(0.27f, 0f, 0f);
         panelAmbar.style.backgroundColor = new Color(0.27f, 0.27f, 0f);
         panelVerde.style.backgroundColor = new Color(0f, 0.27f, 0f);
@@ -49,23 +216,14 @@ public class p4custom : VisualElement
 
     public p4custom()
     {
-    /*
-        VisualElement panelRojo = new VisualElement();
-        VisualElement panelAmbar = new VisualElement();
-        VisualElement panelVerde = new VisualElement();
-    */
-            
         panelRojo.style.width = 100;
         panelRojo.style.height = 100;
-        //panelRojo.style.backgroundColor = Color.red;
 
         panelAmbar.style.width = 100;
         panelAmbar.style.height = 100;
-        //panelAmbar.style.backgroundColor = Color.yellow;
 
         panelVerde.style.width = 100;
         panelVerde.style.height = 100;
-        //panelVerde.style.backgroundColor = Color.green;
 
         styleSheets.Add(Resources.Load<StyleSheet>("p4custom"));
         
@@ -77,4 +235,5 @@ public class p4custom : VisualElement
         hierarchy.Add(panelAmbar);
         hierarchy.Add(panelVerde);
     }
+    */
 }
--- DSI_P04/Assets/p4otra.cs
+++ DSI_P04/Assets/p4otra.cs
@@ -10,35 +10,32 @@ public class p4otra : MonoBehaviour
         VisualElement rootve = GetComponent<UIDocument>().rootVisualElement;
         
         VisualElement top       = rootve.Q("Top");
-        VisualElement bottom    = rootve.Q("Bottom");
 
-        VisualTreeAsset template_cajas = Resources.Load<VisualTreeAsset>("Templates/ContenedorCajas");
+        VisualTreeAsset template_cajas = Resources.Load<VisualTreeAsset>("Templates/Elementos");
 
         VisualElement cajasTop1        = template_cajas.Instantiate();
-        VisualElement cajasTop2        = template_cajas.Instantiate();
-        VisualElement cajasBottom      = template_cajas.Instantiate();
 
         top.Add(cajasTop1);
-        top.Add(cajasTop2);
-        bottom.Add(cajasBottom);
 
-        Sprite img = Resources.Load<Sprite>("Imagenes/georgia");
+        Sprite img0 = Resources.Load<Sprite>("Imagenes/georgia");
+        Sprite img1 = Resources.Load<Sprite>("Imagenes/madrid");
+        Sprite img2 = Resources.Load<Sprite>("Imagenes/chile");
+        Sprite img3 = Resources.Load<Sprite>("Imagenes/cadiz");
+        Sprite img4 = Resources.Load<Sprite>("Imagenes/china");
+        Sprite img5 = Resources.Load<Sprite>("Imagenes/filipinas");
 
-        VisualElement caja1 = cajasTop1.Q("Caja1");
-        VisualElement caja2 = cajasTop2.Q("Caja2");
-        VisualElement caja3 = cajasBottom.Q("Caja3");
+        VisualElement caja0 = cajasTop1.Q("caja0");
+        VisualElement caja1 = cajasTop1.Q("caja1");
+        VisualElement caja2 = cajasTop1.Q("caja2");
+        VisualElement caja3 = cajasTop1.Q("caja3");
+        VisualElement caja4 = cajasTop1.Q("caja4");
+        VisualElement caja5 = cajasTop1.Q("caja5");
 
-        caja1.style.backgroundImage = new StyleBackground(img);
-        caja2.style.backgroundImage = new StyleBackground(img);
-        caja3.style.backgroundImage = new StyleBackground(img);
-
-        /*
-        for (int i = 0; i < 6; i++)
-        {
-            topChild = template_cajas.Instantiate();
-            top.Add(topChild);
-            bottom.Add(template_cajas.Instantiate());
-        }
-        */
+        caja0.style.backgroundImage = new StyleBackground(img0);
+        caja1.style.backgroundImage = new StyleBackground(img1);
+        caja2.style.backgroundImage = new StyleBackground(img2);
+        caja3.style.backgroundImage = new StyleBackground(img3);
+        caja4.style.backgroundImage = new StyleBackground(img4);
+        caja5.style.backgroundImage = new StyleBackground(img5);
     }
 }
